71431ff632396b637e5f241ed061c955d1212520
kassumassu <142390041+kassumassu@users.noreply.github.com>
2024-03-09 20:52:18 +0200

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,7 @@
+alert("Kehittäjät: Henkka ja Kasper!"); 
+function a() {
+    alert("Me myös tehdään joskus youtube kanava nimeltä Pixel Blocks Game");
+} 
+function b() {
+    alert("Muista tilata myös meidän youtube kanava kun se on tehty!")
+}
